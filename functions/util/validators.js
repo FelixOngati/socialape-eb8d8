9bc39a2b28199f99ee4d1860a2dda6f08dc655da
felixongati <fegati02@gmail.com>
2020-06-25 13:36:17 +0300

    adding user details

--- functions/util/validators.js
+++ functions/util/validators.js
@@ -53,6 +53,7 @@ exports.validateLoginData = (data) =>{
 exports.reduceUserDetails = (data) => {
     let userDetails = {};
     if(!isEmpty(data.bio.trim())) userDetails.bio = data.bio.trim();
+    
     if(!isEmpty(data.website.trim())){
         if(data.website.trim().substring(0,4) !== 'http'){
             userDetails.website = `http://${data.website.trim()}`;
