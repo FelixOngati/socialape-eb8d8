4fe735944f34fa03e5df7bf52457edf6fca9adb0
felixongati <fegati02@gmail.com>
2020-06-29 07:19:32 +0300

    getting screams metadata

--- functions/index.js
+++ functions/index.js
@@ -16,6 +16,11 @@ const FBAuth = require('./util/fbAuth');
 //  Scream routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+// TODO: delete scream
+// TODO: like scream
+// TODO: unlike acream
+// TODO: comment scream
 
 // user routes
 app.post('/signup', signup);
